6ab6cf90cff864ae0d95582878e8523138dd42e5
ritam-student <149081707+ritam-student@users.noreply.github.com>
2024-03-22 22:33:15 +0530

    add some commennted code

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -7,3 +7,17 @@ const asyncHandler = () => {
 
 
 export {asyncHandler};
+
+// another way of writting the same
+
+/**const asyncHandler1 = (fn) => async (req,res,next) => {
+    try {
+        
+    } catch (error) {
+        res.status(err.code || 500).json ({
+            success : false,
+            message : err.message
+        })
+    }
+}
+*/
